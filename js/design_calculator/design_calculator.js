19cbd8fbb7e615798e7077582973e1097e3ed2a4
Dalzanor <david.nguyenkhoa@gmail.com>
2021-02-21 21:57:30 -0800

    modified:   js/design_calculator/design_calculator.js 	modified:   js/design_calculator/form_info.js

--- js/design_calculator/design_calculator.js
+++ js/design_calculator/design_calculator.js
@@ -1,12 +1,5 @@
 import { sites, backgrounds, drawings, specialDevices, customers, dropdowns } from "./form_info.js";
 
-// Run the calculateHours on all elements with class "calcOnUpdate"
-for (let i = 0; i < document.getElementsByClassName('calcOnUpdate').length; i++) {
-    document.getElementsByClassName('calcOnUpdate')[i].onchange = () => {
-        calculateHours();
-    }
-}
-
 // Adds the site choices to the Site Information dropdown
 for (let i = 0; i < dropdowns.length; i++) {
     for (let j = 0; j < dropdowns[i].length; j++) {
@@ -22,11 +15,11 @@ for (let i = 0; i < specialDevices.length; i++) {
     const div = document.createElement('div');
     div.className = 'form-check';
     const label = document.createElement('label');
-    label.className = 'form-check-label checkboxDevice';
+    label.className = 'form-check-label';
     label.htmlFor = specialDevices[i].id;
     label.innerHTML = specialDevices[i].name;
     const input = document.createElement('input');
-    input.className = 'form-check-input';
+    input.className = 'form-check-input  checkboxDevice';
     input.type = 'checkbox';
     input.id = specialDevices[i].id;
     div.appendChild(label);
@@ -43,7 +36,7 @@ for (let i = 0; i < customers.length; i++) {
     label.htmlFor = customers[i].id;
     label.innerHTML = customers[i].name;
     const input = document.createElement('input');
-    input.className = 'form-check-input';
+    input.className = 'form-check-input customers';
     input.type = 'radio';
     input.id = customers[i].id;
     input.name = 'customer';
@@ -52,34 +45,104 @@ for (let i = 0; i < customers.length; i++) {
     document.getElementById('radioList').appendChild(div);
 }
 
-// function calculateHours() {
-//     console.log("calculating");
-// }
+// Run the calculateHours on all elements with class "calcOnUpdate"
+for (let i = 0; i < document.getElementsByClassName('calcOnUpdate').length; i++) {
+    document.getElementsByClassName('calcOnUpdate')[i].onchange = () => {
+        document.getElementById('designHours').textContent = Math.ceil(hoursFromSpecialDesign() * hoursFromDropdownMult());
+        document.getElementById('draftHours').textContent = Math.round((hoursFromDevices() + hoursFromSpecialDraft() + hoursFromDropdownDraft()) * customerType() * 2) / 2;
+    }
+}
 
-// function hoursFromDevices() {
-//     for (let i = 0; i < document.getElementsByClassName('deviceCounts'); i++) {
-//         const deviceCounts = 
-//     }
-//     const deviceCounts = document.getElementsByClassName('deviceCounts').value;
-//     let draftTime = 0;
+function hoursFromDevices() {
+    const deviceCounts = document.getElementsByClassName('deviceCounts');
+    for (let i = 0; i < deviceCounts.length; i++) {
+        if (deviceCounts[i].id == 'readerCount') {
+            var readerCount = getValue(deviceCounts[i].value);
+        }
+        else if (deviceCounts[i].id == 'cameraCount') {
+            var cameraCount = getValue(deviceCounts[i].value);
+        }
+        else if (deviceCounts[i].id == 'alarmCount') {
+            var alarmCount = getValue(deviceCounts[i].value);
+        }
+        else if (deviceCounts[i].id == 'panelCount') {
+            var panelCount = getValue(deviceCounts[i].value);
+        }
+    }
 
-//     if (readerCount.value / 16 > panelCount.value) {
-//         draftTime += (Math.ceil(readerCount.value / 16));
-//     }
-//     else {
-//         draftTime += Math.round(panelCount.value);
-//     }
-//     draftTime += Math.ceil(alarmCount.value / 32) + Math.ceil(cameraCount.value / 24);
+    let draftTime = 0;
+    if (readerCount / 16 > panelCount) {
+        draftTime += (Math.ceil(readerCount / 16));
+    }
+    else {
+        draftTime += Math.round(panelCount);
+    }
+    if ((Math.ceil(alarmCount / 24) - Math.round(panelCount)) > 0) {
+        draftTime += (Math.ceil(alarmCount / 24) - Math.round(panelCount));
+    }
+    draftTime += (Math.ceil(cameraCount / 24) / 2);
+    return draftTime;
+}
 
-//     return draftTime;
-// }
+function getValue(value) {
+    if (value == '') {
+        return 0;
+    }
+    else {
+        return value;
+    }
+}
 
-// function hoursFromSpecial() {
-//     for (let i = 0; i < specialDevices.length; i++) {
-//         let s1 = document.getElementById(specialDevices[i].id).checked ? specialDevices[i].value : 0;
-//     }
-// }
+function hoursFromSpecialDraft() {
+    let draftTime = 0;
+    for (let i = 0; i < specialDevices.length; i++) {
+        if (document.getElementsByClassName('checkboxDevice')[i].checked == true) {
+            draftTime += specialDevices[i].draftAddition;
+        }
+    }
+    return draftTime;
+}
 
-// function hoursFromSite() {
+function hoursFromSpecialDesign() {
+    let designTime = 0;
+    for (let i = 0; i < specialDevices.length; i++) {
+        if (document.getElementsByClassName('checkboxDevice')[i].checked == true) {
+            designTime += specialDevices[i].designAddition;
+        }
+    }
+    return designTime;
+}
 
-// }
+function hoursFromDropdownDraft() {
+    let draftTime = 0;
+    for (let i = 0; i < dropdowns.length; i++) {
+        for (let j = 0; j < dropdowns[i].length; j++) {
+            if (document.getElementsByClassName('dropdownMenu')[i].value == dropdowns[i][j].name) {
+                draftTime += dropdowns[i][j].draftAddition;
+            }
+        }
+    }
+    return draftTime;
+}
+
+function hoursFromDropdownMult() {
+    let multiplier = 1;
+    for (let i = 0; i < dropdowns.length; i++) {
+        for (let j = 0; j < dropdowns[i].length; j++) {
+            if (document.getElementsByClassName('dropdownMenu')[i].value == dropdowns[i][j].name) {
+                multiplier *= dropdowns[i][j].draftMultiplier;
+            }
+        }
+    }
+    return multiplier;
+}
+
+function customerType() {
+    let multiplier = 1;
+    for (let i = 0; i < customers.length; i++) {
+        if (document.getElementsByClassName('customers')[i].checked == true) {
+            multiplier *= customers[i].draftMultiplier;
+        }
+    }
+    return multiplier;
+}
--- js/design_calculator/form_info.js
+++ js/design_calculator/form_info.js
@@ -1,27 +1,27 @@
 // Array of site names and the multiplier for hours
 export const sites = [
-    { name: "High-Rise Building", draftAddition: 4 },
-    { name: "Large Site", draftAddition: 3 },
-    { name: "Medium Site", draftAddition: 2 },
-    { name: "Small Site", draftAddition: 1.5 },
-    { name: "MAC", draftAddition: 1 }
+    { name: "High-Rise Building", draftAddition: 4, draftMultiplier: 1 },
+    { name: "Large Site", draftAddition: 3, draftMultiplier: 1 },
+    { name: "Medium Site", draftAddition: 2, draftMultiplier: 1 },
+    { name: "Small Site", draftAddition: 1.5, draftMultiplier: 1 },
+    { name: "MAC", draftAddition: 1, draftMultiplier: 1 }
 ];
 
 // Array of backgrounds and the multiplier for hours
 export const backgrounds = [
-    { name: "Existing CADs", draftMultiplier: 1 },
-    { name: "New CADs", draftMultiplier: 1 },
-    { name: "Clean PDF", draftMultiplier: 1.25 },
-    { name: "PDF/Image", draftMultiplier: 1.66 },
-    { name: "Redraw Layout", draftMultiplier: 2 }
+    { name: "Existing CADs", draftAddition: 0, draftMultiplier: 1 },
+    { name: "New CADs", draftAddition: 0, draftMultiplier: 1 },
+    { name: "Clean PDF", draftAddition: 0, draftMultiplier: 1.25 },
+    { name: "PDF/Image", draftAddition: 0, draftMultiplier: 1.66 },
+    { name: "Redraw Layout", draftAddition: 0, draftMultiplier: 2 }
 ];
 
 // Array of drawing sets and the additional hours for rest of drawing
 export const drawings = [
-    { name: "Full Set", draftAddition: 4 },
-    { name: "DD Only", draftAddition: 1.5 },
-    { name: "CD Only", draftAddition: 1.5 },
-    { name: "RD Only", draftAddition: 1 }
+    { name: "Full Set", draftAddition: 4, draftMultiplier: 1 },
+    { name: "DD Only", draftAddition: 1.5, draftMultiplier: 1 },
+    { name: "CD Only", draftAddition: 1.5, draftMultiplier: 1 },
+    { name: "RD Only", draftAddition: 1, draftMultiplier: 1 }
 ];
 
 // Used for creating dropdown menus
@@ -30,19 +30,19 @@ export const dropdowns = [sites, backgrounds, drawings];
 // Create an array for extraneous devices and the additional hours for special devices
 export const specialDevices = [
     { name: "Door Operator", id: "doorOperatorInfo", draftAddition: 1, designAddition: 2.5 },
-    { name: "Interlock/Mantrap", id: "interlockInfo", value: 3 },
-    { name: "Intrusion Integration", id: "intrusionInfo", value: 1 }, //* multiplier
-    { name: "Tailgate Detector", id: "tailgateInfo", value: 1, },
-    { name: "Turnstile", id: "turnstileInfo", value: 2 },
-    { name: "New Technology Integration", id: "newTechInfo", value: 4 },
+    { name: "Interlock/Mantrap", id: "interlockInfo", draftAddition: 1, designAddition: 2 },
+    { name: "Intrusion Integration", id: "intrusionInfo", draftAddition: 1, designAddition: 0 },
+    { name: "Tailgate Detector", id: "tailgateInfo", draftAddition: 1, designAddition: 0.5 },
+    { name: "Turnstile", id: "turnstileInfo", draftAddition: 1, designAddition: 1 },
+    { name: "New Technology Integration", id: "newTechInfo", draftAddition: 1, designAddition: 3 },
 ];
 
 // New Customer add 25% time
 export const customers = [
-    { name: "New Customer", id: "newCustomer", draftMultiplier: 1.25 },
-    { name: "Existing Customer", id: "existingCustomer", draftMultiplier: 1 },
-    { name: "Apple", id: "appleCustomer", draftMultiplier: .9 },
-    { name: "Facebook", id: "facebookCustomer", draftMultiplier: .85 }
+    { name: "New Customer", id: "newCustomer", draftMultiplier: 1 },
+    { name: "Existing Customer", id: "existingCustomer", draftMultiplier: .8 },
+    // { name: "Apple", id: "appleCustomer", draftMultiplier: .9 }, Just Example not real values
+    // { name: "Facebook", id: "facebookCustomer", draftMultiplier: .85 } Just Example not real values
 ];
 
 
